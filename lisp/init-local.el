6ba2d4546860d74f9ae4d7914dcd5d1130787623
einhep <einhep@gmail.com>
2025-04-01 10:04:36 +0800

    disable proxy

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -6,46 +6,46 @@
 
 ;;; Code:
 
-(setq url-proxy-services
-      '(("no_proxy" . "^\\(localhost\\|10.*\\)")
-        ("http" . "http://127.0.0.1:10077")
-        ("https" . "https://127.0.0.1:10077")))
+;; (setq url-proxy-services
+;;       '(("no_proxy" . "^\\(localhost\\|10.*\\)")
+;;         ("http" . "http://127.0.0.1:10077")
+;;         ("https" . "https://127.0.0.1:10077")))
 
-(defun proxy-socks-show ()
-  "Show SOCKS proxy."
-  (interactive)
-  (when (fboundp 'cadddr)
-    (if (bound-and-true-p socks-noproxy)
-        (message "Current SOCKS%d proxy is %s:%d"
-                 (cadddr socks-server) (cadr socks-server) (caddr socks-server))
-      (message "No SOCKS proxy"))))
+;; (defun proxy-socks-show ()
+;;   "Show SOCKS proxy."
+;;   (interactive)
+;;   (when (fboundp 'cadddr)
+;;     (if (bound-and-true-p socks-noproxy)
+;;         (message "Current SOCKS%d proxy is %s:%d"
+;;                  (cadddr socks-server) (cadr socks-server) (caddr socks-server))
+;;       (message "No SOCKS proxy"))))
 
-(defun proxy-socks-enable ()
-  "Enable SOCKS proxy."
-  (interactive)
-  (require 'socks)
-  (setq url-gateway-method 'socks
-        socks-noproxy '("localhost")
-        socks-server '("Default server" "127.0.0.1" 12377 5))
-  (setenv "all_proxy" "socks5://127.0.0.1:12377")
-  (proxy-socks-show))
+;; (defun proxy-socks-enable ()
+;;   "Enable SOCKS proxy."
+;;   (interactive)
+;;   (require 'socks)
+;;   (setq url-gateway-method 'socks
+;;         socks-noproxy '("localhost")
+;;         socks-server '("Default server" "127.0.0.1" 12377 5))
+;;   (setenv "all_proxy" "socks5://127.0.0.1:12377")
+;;   (proxy-socks-show))
 
-(defun proxy-socks-disable ()
-  "Disable SOCKS proxy."
-  (interactive)
-  (require 'socks)
-  (setq url-gateway-method 'native
-        socks-noproxy nil)
-  (setenv "all_proxy" "")
-  (proxy-socks-show))
+;; (defun proxy-socks-disable ()
+;;   "Disable SOCKS proxy."
+;;   (interactive)
+;;   (require 'socks)
+;;   (setq url-gateway-method 'native
+;;         socks-noproxy nil)
+;;   (setenv "all_proxy" "")
+;;   (proxy-socks-show))
 
-(defun proxy-socks-toggle ()
-  "Toggle SOCKS proxy."
-  (interactive)
-  (require 'socks)
-  (if (bound-and-true-p socks-noproxy)
-      (proxy-socks-disable)
-    (proxy-socks-enable)))
+;; (defun proxy-socks-toggle ()
+;;   "Toggle SOCKS proxy."
+;;   (interactive)
+;;   (require 'socks)
+;;   (if (bound-and-true-p socks-noproxy)
+;;       (proxy-socks-disable)
+;;     (proxy-socks-enable)))
 
 (window-numbering-mode t)
 
